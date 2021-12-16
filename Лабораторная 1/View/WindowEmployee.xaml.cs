2f1bee619651b6c55885aaf9ed76dfb1dc905f8f
Ярослав Дрововозов <byster1234@mail.ru>
2021-12-16 11:15:42 +0300

    Добавьте файлы проекта.

---
+++ Лабораторная 1/View/WindowEmployee.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using Лабораторная_1.ViewModel;
+using Лабораторная_1.Model;
+using Лабораторная_1.Helper;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Лабораторная_1.View
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowEmployee.xaml
+    /// </summary>
+    public partial class WindowEmployee : Window
+    {
+        public WindowEmployee()
+        {
+            InitializeComponent();
+            DataContext = new PersonViewModel();
+        }
+    }
+}
---
+++ Лабораторная 1/View/WindowRole.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using Лабораторная_1.Model;
+using Лабораторная_1.ViewModel;
+
+namespace Лабораторная_1.View
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowRole.xaml
+    /// </summary>
+    public partial class WindowRole : Window
+    {
+        public WindowRole()
+        {
+            InitializeComponent();
+            DataContext = new RoleViewModel();
+        }
+    }
+}
